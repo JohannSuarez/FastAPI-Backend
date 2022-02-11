f690245dab51395983e4cd7e0438d631a34e9067
JohannSuarez <johann.suarez92@gmail.com>
2022-02-10 23:16:27 -0800

    Finished EveVy Image Uploading Service

--- blog/services/eyevy.py
+++ blog/services/eyevy.py
@@ -1,55 +1,62 @@
-from fastapi import FastAPI, UploadFile, APIRouter
 from pathlib import Path
+from PIL import Image
+from ..utils.app_exceptions import AppException
 
+import shutil
 
 """
-What should we put into this class?
-What should we receive from this class?
-
-EyevyService encapsulates all business logic of the service.
 The return value are of type ServiceResult: Containing a value attribute
 with returnable data or, in case of an exception, an AppException.
-
-In both cases, the respective result is returned back "upwards" to the controller layer.
-
-
-handle_result() that is invoked in Router expects either a ServiceResult. If ServiceResult.success
-we return the entire ServiceResult
-
-If the ServiceResult.success if false, ServiceResult literally just returns an exception.
-otherise, it returns a non-specified data type (but most likely a JSON) that can be accessed
-with the with statement.
-
 """
 
 from ..utils.app_exceptions import AppException
 from ..utils.service_result import ServiceResult
 
 class EyevyService():
-    """
-    What is the service?
-    We either return a ServiceResult fed with valid return data,
-    or ServiceResult fed with Exception.
-    """
 
-    @staticmethod
-    def receive_image(image):
-        pass
+    def image_handler(self, image) -> ServiceResult:
+        '''
+        Receives Image, first verifies.
+        If verified, save image to server.
+        '''
+        validation_error: ServiceResult | None = self.is_invalid(image)
 
-    @staticmethod
-    def verfication():
-        pass
+        if validation_error:
+            return validation_error
+
+        return self.write(image)
 
 
-    def write(image):
+    def is_invalid(self, image) -> None | ServiceResult:
+        '''
+        If the image is invalid, it'll return a ServiceResult,
+        otherwise we expect nothing.
+        '''
+
+        # Open image with PIL, use verify() to check if actually an image file.
+        try:
+            im = Image.open(image.file)
+            im.verify()
+        except Exception:
+            context: dict = {"Format Error": f"{image.filename} file is not an image."}
+            return ServiceResult(AppException.ImageVerification(context))
+
+
+    def write(self, image) -> ServiceResult:
+        """
+        Save the image into the server, unless
+        an error is raised along the way.
+        """
 
         filename: Path = Path(image.filename)
         directory: str = "received"
+        absolute_dir: Path = Path(__file__).parent.resolve()
 
         try:
-            with open(f"{directory}/{filename.with_suffix('.png')}", "wb") as buffer:
+            with open(f"{str(absolute_dir)}/{directory}/{filename.with_suffix('.png')}", "wb") as buffer:
                 shutil.copyfileobj(image.file, buffer)
-        except Exception as e:
-            return e
+        except Exception:
+            context: dict = {"Write Error": "Something went wrong during image save step."}
+            return ServiceResult(AppException.ImageSaving(context))
 
-        return ServiceResult(write_res)
+        return ServiceResult({"Success":"Image Saved"})
